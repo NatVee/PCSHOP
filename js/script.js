34ebf7ea327e9d6d1799a8289fda1d2b6af37d8e
NatVee <102380124+NatVee@users.noreply.github.com>
2023-03-22 19:52:24 +0300

    Create slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-  });
-  
+  // Your custom options
+});
+
+
+const reviewSlider = new Swiper('.review-slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    }
+  },
+
+  loop: true,
+
+  navigation: {
+    nextEl: '.review-slider__btn-next',
+    prevEl: '.review-slider__btn-prev',
+  },
+});  
